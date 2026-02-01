526f2d4890bdebd15b69fdf181bcc0090460f222
Malin502 <takuto.ponta@gmail.com>
2026-02-01 16:27:55 +0900

    デザインと型定義

---
+++ src/types/index.ts
@@ -0,0 +1,72 @@
+//ナビゲーションメニュー
+export interface NavItem {
+  label: string;
+  href: string;
+};
+
+//プロフィール情報
+export interface Profile {
+  nameEn: string;
+  nameJp: string
+  role: string;
+  description: string;
+};
+
+//スキル
+export interface Skill {
+  name: string;
+  icon: React.ElementType;
+  level: number; // 1から5
+  experience: string; // 経験期間
+description: string;
+}
+
+//資格
+export interface Certification {
+  id: string;
+  name: string;
+  date: string;
+  icon?: React.ElementType;
+}
+
+//実務経験
+export interface Experience {
+    id: string;
+    period: string;
+    title: string;
+    company: string;
+    description: string;
+    skills: string[]; //使用技術 ["AWS","React","TypeScript"]
+}
+
+//制作実績
+export interface Work {
+    id: string;
+    title: string;
+    description: string;
+    imageUrl: string;
+    tags: string[];
+    link?: string;
+    githubUrl?: string;
+}
+
+//研究
+export interface Research {
+    id: string;
+    title: string;
+    highlights: {
+        title: string;
+        description: string;
+    }[];
+    overview: string;
+    imageUrl?: string;
+    link?: string;
+}
+
+//Future
+export interface FutureGoal {
+    id: string;
+    label: "Now" | "Next" | "Future";
+    title: string;
+    description: string;
+}
